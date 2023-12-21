90f91513f97e27277869f92a7e9afb4c5eb44824
LydiaYoon <narae456@gmail.com>
2023-12-21 17:01:49 +0900

    feat: [메인 페이지] Slide Carousel 생성

---
+++ src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+const sliderData = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1523275335684-37898b6baf30?q=80&w=2899&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 1',
+    desc: 'description 1',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 2',
+    desc: 'description 2',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=2881&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    heading: 'title 3',
+    desc: 'description 3',
+  },
+];
+
+export default sliderData;
